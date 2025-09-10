022d53f108c019008b0bffa1b4bc37d7ca8212ef
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2025-09-10 09:28:05 +0200

    upgrade

---
+++ 02_adattipusok.py
@@ -0,0 +1,18 @@
+
+
+
+name = "Bulyaki Bence"
+age = 17
+height = 1.92
+is_student = True
+#print(f'Name: {name},  \nAge: {age}, \nHeight: {height}, \nIs student: {is_student}')
+print(f"""Name: {name},
+Age: {age}, 
+Height: {height}, 
+Is student: {is_student}""")
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
---
+++ helloworld_short.py
@@ -0,0 +1,4 @@
+lastname = input ("Enter your lastname ")
+firstname = input ("Enter your firstname ")
+age = input ("Enter your age ")
+print(f"Hello, {firstname} {lastname}! {age} ")
